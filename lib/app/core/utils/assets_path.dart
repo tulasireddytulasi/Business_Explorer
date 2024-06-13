18a23b11025b3acfce03d656525cc0c72c5333b6
Tulasi Reddy <tulasireddy.kagithala@karkinos.in>
2024-06-13 13:36:25 +0530

    Updated Changes

--- lib/app/core/utils/assets_path.dart
+++ lib/app/core/utils/assets_path.dart
@@ -1,18 +1,7 @@
 class Assets {
   Assets._();
 
-  static const String abdmLogo = "assets/images/abdm_logo.svg";
-  static const String karkinosLogo = "assets/images/karkinos_logo.svg";
-  static const String abhaCard = "assets/images/abha_card.svg";
-  static const upArrow = "assets/images/up_arrows.png";
-  static const chain = "assets/images/chain.png";
-
-
   /// Icons
   static const String coconutTree = "assets/icons/coconut_tree.png";
 
-
-  /// Lottie Animations
-  static const String pinkLoader = "assets/lottie/pink_loader.json";
-  static const String blueLoader = "assets/lottie/blue_loader.json";
 }
